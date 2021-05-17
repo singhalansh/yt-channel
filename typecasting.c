737373ee92922abc463170e1f02c0401011576fc
singhalansh <82322553+singhalansh@users.noreply.github.com>
2021-05-17 16:47:17 +0530

    Add files via upload

---
+++ typecasting.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    // typecasting syntax
+    // (type) variable;
+    int a=10;
+    float b = (float)54/10;
+    printf("the value of a is %d and b is %f",a,b);
+    
+    return 0;
+}
