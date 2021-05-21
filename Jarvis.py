0b75beeb7bfa8a538bc53e70c08c1d426a07a46e
singhalansh <82322553+singhalansh@users.noreply.github.com>
2021-05-21 18:41:23 +0530

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -1,9 +1,4 @@
 import speech_recognition as sr
-import pyttsx3
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-print(voices[2].id)
-engine.setProperty('voice',voices[2].id)
 def listen():
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -17,7 +12,4 @@ def listen():
             print("voice not recognised ...... please try again!")
         return query
 
-def say(str):
-    engine.say(str)
-    engine.runAndWait()
-say("hello there how are you")
+print(listen())
