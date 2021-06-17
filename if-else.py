110a27c77cf67552a1d71d68ddb7df12c75f4534
singhalansh <82322553+singhalansh@users.noreply.github.com>
2021-06-17 19:49:19 +0530

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,76 @@
+#if x= 1 then print("ansh is good boy")
+
+#or if x is not equal to 1,
+#then print("manish is a bad boy")
+
+''' operators
+arithematic operator
++
+-
+*
+/
+**
+//
+%
+
+comparision operator
+==
+<
+>
+<=
+>=
+!=
+assignment operator
+=
++= x+=1  ==> x= x+1
+-= x-=1  ==> x = x-1
+*= x*=1  ==> x = x*1
+/= x/=1  ==> x=x/1
+'''
+'''x = int(input("enter number"))
+if(x==1):
+    print("ansh is a good boy")
+else:
+    print("manish is a bad boy")'''
+
+'''x="ansh "
+if x=="ansh ":
+    print(x*3)
+else:
+    print("not equal, so user is good enough")'''
+'''x=input("enter your name")
+if x=="ansh":
+    print("user is ansh singhal")
+elif x=="manish":
+    print("ansh has entered name manish")
+else:
+    print("the name is not in database")'''
+
+
+'''x= int(input("enter number" ))
+
+if x==1:
+    print("it's sunday funday")
+elif x==2:
+    print("it's monday")
+elif x==3:
+    print("it's tuesday")
+elif x==4:
+    print("it's wednesday")
+elif x==5:
+    print("it's thursday")
+elif x==6:
+    print("it's friday")
+elif x==7:
+    print("it's saturday")
+else:
+    print("enter valid number")'''
+    
+
+
+
+
+    
+    
+
+
