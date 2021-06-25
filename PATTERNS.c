331aaa1d70a5ae7f3996169d0c7bd7b1f575574a
singhalansh <82322553+singhalansh@users.noreply.github.com>
2021-06-25 19:43:13 +0530

    Add files via upload

---
+++ PATTERNS.c
@@ -0,0 +1,128 @@
+#include<stdio.h>
+int main()
+{
+    /*
+       *
+      ***
+     *****
+    *******
+    */
+  
+  /* for(int i=0; i<4;i++){
+       for(int j=3;j>i;j--){
+           printf(" ");
+        
+       }
+       for(int k=i+2;k>1;k--){
+           printf("*");
+       }
+       for(int l=i;l>0;l--){
+           printf("*");
+       }
+       printf("\n");
+   }*/
+   /*
+   *********
+   ********
+   *******
+   ******
+   *****
+   ****
+   ***
+   **
+   *
+    */
+ /*  for(int i=10;i>0;i--){
+       for(int j=0;j<i;j++){
+           printf("*");
+           }
+        printf("\n");
+   }
+*/
+    /*
+       *
+      ***
+     *****
+    *******
+     *****
+      ***
+       *
+    */
+   /* for(int i=0; i<4;i++){
+        for(int j=3;j>i;j--){
+            printf(" ");
+        
+       }
+        for(int k=i+2;k>1;k--){
+            printf("*");
+       }
+        for(int l=i;l>0;l--){
+            printf("*");
+       }
+        printf("\n");}
+    for(int i=4; i>1;i--){
+        for(int j=5;j>i;j--){
+            printf(" ");
+        
+       }
+        for(int k=i;k>1;k--){
+            printf("*");
+       }
+        for(int l=i;l>2;l--){
+            printf("*");
+       }
+        printf("\n");}*/
+/* 
+  1
+ 121
+12321
+ 121
+  1
+*/
+/*
+for(int i=0; i<3;i++){
+       for(int j=3;j>i+1;j--){
+           printf(" ");
+        
+       }
+       for(int k=1;k<i+1;k++){
+           printf("%d",k);
+       }
+       for(int l=i+1;l>0;l--){
+           printf("%d",l);
+       }
+       printf("\n");}
+
+
+for(int i=2; i>0;i--){
+       for(int j=3;j>i;j--){
+           printf(" ");
+        
+       }
+       for(int k=1;k<i;k++){
+           printf("%d",k);
+       }
+       for(int l=i;l>0;l--){
+           printf("%d",l);
+       }
+       printf("\n");}*/
+    
+
+/* 1 1 2 3 5 8 13 21 .........*/
+    /*int num,start,n;
+    
+    start = 1;
+    printf("enter the number please: \n");
+    scanf("%d",&num);
+    int arr1[num+1];
+    arr1[0]=1;
+    for(int i=0;i<num;i++){
+
+        printf("%d ",arr1[i]);
+        arr1[i+1]=start;
+        start += arr1[i];  }
+*/
+  
+return 0;
+}
+
